37618e16005b6f170b618b948f53240d569523fe
gorodilin.m <gorodilin.m@partner.samsung.com>
2020-09-30 17:18:27 +0300

    Trigger rebuild.

--- SimpleBackend/Program.cs
+++ SimpleBackend/Program.cs
@@ -6,7 +6,7 @@ namespace SimpleBackend
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! and trigger");
             Console.ReadKey();
         }
     }
